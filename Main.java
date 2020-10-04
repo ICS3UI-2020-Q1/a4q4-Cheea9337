3beefa0b74988bfa4dddbe7619859d1c19dc4978
Adam Lamont <adam_lamont@wrdsb.ca>
2020-10-04 15:25:41 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
  *
- * @author 
+ * @author Aidan Cheesmond
  */
 public class Main {
 
@@ -9,7 +10,18 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create a scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    // declare a variable
+    int userNum;
+    do{
+      System.out.println("Please enter a positive integer to see the FizzBuzz numbers");
+      userNum = input.nextInt();
+    }while
+
+
+
     
   }
 }
